2019bf7701f4bf1a5ad41d12e38961bdc4a154a4
Lauren Brender <lkbrender@mail.com>
2026-09-14 19:17:02 +0100

    changed name of file, addied additional packets

---
+++ Sandbox/blockSandbox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+public class BlockSandbox
+{
+    public static void ExecuteSafeCode(string generatedSourceCode)
+    {
+        // 1. Only reference essential, safe assemblies (No System.IO, System.Net, etc.)
+        var trustedAssemblies = new[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // System.Private.CoreLib
+            MetadataReference.CreateFromFile(typeof(Console).Assembly.Location)  // System.Console
+        };
+
+        // 2. Parse the code with strict compilation choices
+        var syntaxTree = CSharpSyntaxTree.ParseText(generatedSourceCode);
+        var compilation = CSharpCompilation.Create("UserBlocksAssembly")
+            .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+            .AddReferences(trustedAssemblies)
+            .AddSyntaxTrees(syntaxTree);
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        if (!result.Success)
+        {
+            // Handle compilation errors (e.g., block validation failed)
+            return;
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        // 3. Load into an isolated AssemblyLoadContext for easy garbage collection
+        var alc = new AssemblyLoadContext("UserCodeContext", isCollectible: true);
+        try
+        {
+            Assembly assembly = alc.LoadFromStream(ms);
+            
+            // Look up the entry method you generated (e.g., UserScript.Run)
+            var type = assembly.GetType("UserScript");
+            var method = type?.GetMethod("Run");
+            
+            // Execute the block code
+            method?.Invoke(null, null);
+        }
+        finally
+        {
+            // 4. Clean up and unload the memory so old block runs don't leak memory
+            alc.Unload();
+        }
+    }
+}
--- Sandbox/obj/Debug/net9.0/Sandbox.AssemblyInfo.cs
+++ Sandbox/obj/Debug/net9.0/Sandbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b512474556782966f3461cefdb67bf958758bd9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
